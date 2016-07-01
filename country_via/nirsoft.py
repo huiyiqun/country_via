fe65fc72c9aa4c2237b82529f3e9dbe821519f61
Hui Yiqun <huiyiqun@gmail.com>
2016-07-01 16:27:17 +0800

    fix code style

--- country_via/nirsoft.py
+++ country_via/nirsoft.py
@@ -1,10 +1,8 @@
-import csv
 import requests
-import itertools
 
-from netaddr import cidr_merge, iprange_to_cidrs
 from ipaddress import (summarize_address_range,
                        collapse_addresses, IPv4Address)
+
 
 class AddressBlock:
     def __init__(self, country_code):
@@ -13,11 +11,18 @@ class AddressBlock:
     @property
     def blocks(self):
         if not hasattr(self, '_blocks'):
+            blocks = []
             res = requests.get(
-                'http://www.nirsoft.net/countryip/{}.csv'.format(self.country_code))
-            raw_blocks = [row.split(',') for row in res.text.split('\r\n') if row]
-            self._blocks = [{'start': row[0], 'end': row[1]} for row in raw_blocks]
-
+                'http://www.nirsoft.net/countryip/{}.csv'.format(
+                    self.country_code))
+            for row in res.text.split('\r\n'):
+                if row:
+                    raw_block = row.split(',')
+                    blocks.append({
+                        'start': raw_block[0],
+                        'end': raw_block[1]
+                    })
+            self._blocks = blocks
         return self._blocks
 
     @property
@@ -28,11 +33,11 @@ class AddressBlock:
                 self._networks += summarize_address_range(
                     IPv4Address(block['start']),
                     IPv4Address(block['end']))
-            self._networks += collapse_addresses(self._networks)
+            self._networks = list(collapse_addresses(self._networks))
         return self._networks
-
 
 
 if __name__ == '__main__':
     for network in AddressBlock('cn').networks:
         print(network.with_prefixlen)
+    print(len(AddressBlock('cn').networks))
