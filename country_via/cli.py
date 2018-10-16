5167bdb37ba053c6ff6dd640c1268c391df9effd
Hui Yiqun <i@huiyiqun.me>
2018-10-16 11:33:00 +0800

    add support for selecting routing table

--- country_via/cli.py
+++ country_via/cli.py
@@ -11,12 +11,14 @@ def parse_args(*args, **kwargs):
                         required=True, dest='country_code')
 
     variables_group = parser.add_argument_group('variables')
-    variables_group.add_argument('--via', '-v', help='gateway for the country',
+    variables_group.add_argument('--via', '-v', help='the gateway for the country',
                         dest='via')
-    variables_group.add_argument('--dev', '-d', help='device name of gateway',
+    variables_group.add_argument('--dev', '-d', help='the device name of gateway',
                         dest='dev')
-    variables_group.add_argument('--type', '-t', help='routing type',
+    variables_group.add_argument('--type', '-t', help='the routing type',
                         dest='type')
+    variables_group.add_argument('--table', help='the routing table number to be operated',
+                        dest='table')
 
     parser.add_argument('--format', '-f', help='the format of the output', default='ip_route',
                         choices=['ip_route', 'systemd_network', 'plain_list'], dest='format')
@@ -29,5 +31,6 @@ def country_via():
         'via': args.via,
         'dev': args.dev,
         'type': args.type,
+        'table': args.table,
     })
     print(renderer.render(args.format))
